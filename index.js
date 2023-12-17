583a4d88349630049e2a1faae0cc29a034ae88d1
Yiannis <grigogiannis@gmail.com>
2023-12-17 21:00:52 +0200

    Project of blog web app with features to write, post and read articles

---
+++ index.js
@@ -0,0 +1,111 @@
+import express from "express";
+import bodyParser from "body-parser";
+import name from "ejs";
+import multer from 'multer';
+
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+
+app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
+
+// Array to store posts in memory
+const postsArray = [];
+
+
+
+// Mutler set up
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/'); // Store files in the 'uploads' directory
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname); // Use a unique filename
+    },
+  });
+  
+const upload = multer({ storage: storage });
+
+app.use((err, req, res, next) => {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      res.status(500).send('Multer error: ' + err.message);
+    } else if (err) {
+      // An unknown error occurred.
+      res.status(500).send('Error: ' + err.message);
+    } else {
+      // No error occurred, proceed to the next middleware.
+      next();
+    }
+  });
+
+app.get("/", (req, res) => {    
+    res.render("index.ejs");   
+});
+
+app.get("/submit-post", (req, res) => {
+    res.render("submit-post.ejs");
+});
+
+app.get("/posts", (req, res) => {
+    const postsArraySize = postsArray.length;
+    res.render("posts.ejs", { numOfPosts: postsArraySize, postsArray });
+    console.log(postsArraySize);
+});
+
+// Handle POST request for submitting a post
+app.post("/submit-post", upload.single("file"), (req, res) => {
+    const postTitle = req.body["title"].trim();
+    const postAuthor = req.body["author"].trim();
+    const postText = req.body["post-text"];
+    const postImage = req.file;
+
+    // Store the post in the array
+    const newPost = { title: postTitle, author: postAuthor, text: postText, image: postImage };
+    postsArray.push(newPost);
+
+    console.log("Posts Array:", postsArray); // Log the posts array
+
+    // Redirect to the submited post page
+    res.redirect("/posted");
+});
+
+app.get("/posted", (req, res) => {
+    res.render("posted.ejs", { postsArray });
+});
+
+app.get("/:title", (req, res) => {
+    const requestedTitle = req.params.title;
+    // const requestedAuthor = req.params.author;
+
+    console.log("Requested Title:", requestedTitle);
+    // console.log("Requested Author:", requestedAuthor);
+
+    // Find the post in the array based on title and author
+    const requestedPost = postsArray.find(post => (
+        post.title.toLowerCase().replace(/\s+/g, '-') === requestedTitle.toLowerCase() 
+        // && post.author.toLowerCase() === requestedAuthor.toLowerCase()
+      ));
+      
+
+    console.log("Requested Post:", requestedPost);
+
+    if (requestedPost) {
+        res.render("view-post.ejs", { post: requestedPost });
+    } else {
+        // Handle post not found
+        res.status(404).send("Post not found");
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
